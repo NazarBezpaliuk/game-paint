c9544ffede903638bc0bc3ae2c55b02e8b9418bd
NazarBezpaliuk <nazardxvx@gmail.com>
2022-12-28 16:39:55 +0200

    lock

--- lock.js
+++ lock.js
@@ -1,35 +1,35 @@
-// const wraper = document.querySelector("#wraper");
-// const input = document.querySelector("#locker_input");
-// const button = document.querySelector("#locker_submit");
-// const open = document.querySelector(".aaa");
-// const opt = document.querySelector(".aet");
-// const zamok = document.querySelector(".zamok");
+const wraper = document.querySelector("#wraper");
+const input = document.querySelector("#locker_input");
+const button = document.querySelector("#locker_submit");
+const open = document.querySelector(".aaa");
+const opt = document.querySelector(".aet");
+const zamok = document.querySelector(".zamok");
 
-// const PASSWORD = "2819";
+const PASSWORD = "2819";
 
-// const unlock = () => {
-//   if (input.value === PASSWORD) {
-//     open.classList.add("openlock");
-//     return wraper.classList.add("wraper_unlock");
-//   }
-//   zamok.classList.add("shot");
-//   opt.classList.add("ppp");
-//   wraper.classList.add("wraper_lock");
-//   input.classList.add("locker_input_lock");
-// };
+const unlock = () => {
+  if (input.value === PASSWORD) {
+    open.classList.add("openlock");
+    return wraper.classList.add("wraper_unlock");
+  }
+  zamok.classList.add("shot");
+  opt.classList.add("ppp");
+  wraper.classList.add("wraper_lock");
+  input.classList.add("locker_input_lock");
+};
 
-// const unlockKey = (event) => {
-//   if (event.code === "Enter") {
-//     if (input.value === PASSWORD) {
-//       open.classList.add("openlock");
-//       return wraper.classList.add("wraper_unlock");
-//     }
-//     zamok.classList.add("shot");
-//     opt.classList.add("ppp");
-//     wraper.classList.add("wraper_lock");
-//     input.classList.add("locker_input_lock");
-//   }
-// };
+const unlockKey = (event) => {
+  if (event.code === "Enter") {
+    if (input.value === PASSWORD) {
+      open.classList.add("openlock");
+      return wraper.classList.add("wraper_unlock");
+    }
+    zamok.classList.add("shot");
+    opt.classList.add("ppp");
+    wraper.classList.add("wraper_lock");
+    input.classList.add("locker_input_lock");
+  }
+};
 
-// button.addEventListener("click", unlock);
-// input.addEventListener("keydown", unlockKey);
+button.addEventListener("click", unlock);
+input.addEventListener("keydown", unlockKey);
