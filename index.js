d0049544cb6d3265bd27a2a0bfd18d9a7050c7e7
NazarBezpaliuk <nazardxvx@gmail.com>
2022-12-30 20:43:37 +0200

    1

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-// const brown = document.querySelector("#brown");
-// const green = document.querySelector("#green");
-// const black = document.querySelector("#black");
-// const blue = document.querySelector("#blue");
-// const yellow = document.querySelector("#yellow");
-// const red = document.querySelector("#red");
-// const gray = document.querySelector("#gray");
 const roof = document.querySelector("#roof");
 const wall = document.querySelector("#wall");
 const door = document.querySelector("#door");
@@ -13,9 +6,51 @@ const resetBtn = document.querySelector("#reset-btn");
 const colorg = document.querySelector("#tyui");
 const ccc = document.querySelector("#tyu");
 const colort = document.querySelector("#colort");
+const nextBtn = document.querySelector("#btn-next");
+const smile = document.querySelector("#smile");
+const bodySmile = document.querySelector("#cirkle-smile");
+const eyseLeft = document.querySelector("#eyse-left");
+const eyseRight = document.querySelector("#eyse-right");
+const smileBottom = document.querySelector("#smile_bottom");
+const prewBtn = document.querySelector("#btn-prew");
 
 let saveColor = colorg.value;
 colort.style.border = `15px solid ${saveColor}`;
+
+let roofPosition = -50;
+let wallPosition = -50;
+let windowPosition = -150;
+let doorPosition = 0;
+let smilePosition = 950;
+
+const nextSlide = () => {
+  roofPosition += -1000;
+  wallPosition += -1000;
+  windowPosition += -1000;
+  doorPosition += -1000;
+  smilePosition += -1000;
+  smile.style.transform = `translate(${smilePosition}%, -40%)`;
+  roof.style.transform = `translate(${roofPosition}%, -200%) perspective(120px) rotateX(45deg)`;
+  wall.style.transform = `translate(${wallPosition}%, -35%)`;
+  windowss.style.transform = `translate(${windowPosition}%, -50%)`;
+  door.style.transform = `translate(${doorPosition}%, -6px)`;
+};
+
+const prewSlide = () => {
+  roofPosition -= -1000;
+  wallPosition -= -1000;
+  windowPosition -= -1000;
+  doorPosition -= -1000;
+  smilePosition -= -1000;
+  smile.style.transform = `translate(${smilePosition}%, -40%)`;
+  roof.style.transform = `translate(${roofPosition}%, -200%) perspective(120px) rotateX(45deg)`;
+  wall.style.transform = `translate(${wallPosition}%, -35%)`;
+  windowss.style.transform = `translate(${windowPosition}%, -50%)`;
+  door.style.transform = `translate(${doorPosition}%, -6px)`;
+};
+
+nextBtn.addEventListener("click", nextSlide);
+prewBtn.addEventListener("click", prewSlide);
 
 const changeCCC = () => {
   saveColor = `${colorg.value}`;
@@ -23,6 +58,18 @@ const changeCCC = () => {
 };
 colorg.addEventListener("input", changeCCC);
 
+const bodyColor = () => {
+  bodySmile.style.backgroundColor = saveColor;
+};
+const eyseLeftColor = () => {
+  eyseLeft.style.backgroundColor = saveColor;
+};
+const eyseRightColor = () => {
+  eyseRight.style.backgroundColor = saveColor;
+};
+const smileColor = () => {
+  smileBottom.style.backgroundColor = saveColor;
+};
 const roofColor = () => {
   roof.style.backgroundColor = saveColor;
 };
@@ -36,6 +83,10 @@ const windowssColor = () => {
   windowss.style.backgroundColor = saveColor;
 };
 
+bodySmile.addEventListener("click", bodyColor);
+eyseLeft.addEventListener("click", eyseLeftColor);
+eyseRight.addEventListener("click", eyseRightColor);
+smileBottom.addEventListener("click", smileColor);
 roof.addEventListener("click", roofColor);
 wall.addEventListener("click", wallColor);
 door.addEventListener("click", doorColor);
@@ -46,5 +97,9 @@ const resetColor = () => {
   wall.style.backgroundColor = "#fff";
   door.style.backgroundColor = "#fff";
   windowss.style.backgroundColor = "#fff";
+  bodySmile.style.backgroundColor = "#fff";
+  eyseLeft.style.backgroundColor = "#fff";
+  eyseRight.style.backgroundColor = "#fff";
+  smileBottom.style.backgroundColor = "#fff";
 };
 resetBtn.addEventListener("click", resetColor);
